6c6762e3b66c6776bd886f5d8d20f4bf6dd21f96
rclemsmith <rclemsmith@gmail.com>
2023-10-03 15:04:48 +0530

    Get Set Go

---
+++ interfaces/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type shape interface {
+	area() float64
+}
+
+type circle struct {
+	radius float64
+}
+
+type rect struct {
+	width  float64
+	height float64
+}
+
+func (r rect) area() float64 {
+	return r.height * r.width
+}
+
+func (c circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+
+func getArea(s shape) float64{
+	return s.area()
+}
+
+func main() {
+	c1 := circle{4.5}
+	r1 := rect{4, 5}
+
+	shapes := []shape{c1, r1}
+
+	for _, shape := range shapes {
+		fmt.Println(shape.area())
+	}
+
+	for _, shape := range shapes {
+		fmt.Println(getArea(shape))
+	}
+}
