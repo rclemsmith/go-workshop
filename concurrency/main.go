0d97c639cbe48f368032cabf610da70a4ce7e181
rclemsmith <rclemsmith@gmail.com>
2023-10-03 16:09:30 +0530

    Get Set Go

---
+++ concurrency/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+var signals = []string{"Test"}
+
+var wg sync.WaitGroup // Pointer
+
+var mut sync.Mutex 
+
+func main() {
+	// go greeter("Hello")
+	// go greeter("World")
+
+	websitelist := []string{
+		"https://lco.dev",
+		"https://go.dev",
+		"https://google.com",
+		"https://github.com",
+		"https://fb.com",
+	}
+
+	for _,web := range websitelist{
+		go getStatusCode(web)
+		wg.Add(1)
+	}
+
+	wg.Wait()
+	fmt.Println(signals)
+}
+
+func getStatusCode(endpoint string){
+	defer wg.Done()
+	res,err := http.Get(endpoint)
+	
+	if err != nil {
+		fmt.Println("OOPS in Endpoint")
+	} else {
+		mut.Lock()
+		signals = append(signals, endpoint)
+		mut.Unlock()
+		fmt.Printf("%d Status Code for website for %s\n", res.StatusCode,endpoint)
+	}
+
+	
+}
+
+func greeter(s string) {
+	for i := 0; i < 6; i++ {
+		fmt.Println(s)
+	}
+}
